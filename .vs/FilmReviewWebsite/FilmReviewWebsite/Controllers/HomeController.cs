a96b9e87b30db74498f174668010aa5b40651679
Weijie Ye <P17226039@my365.dmu.ac.uk>
2019-03-25 16:42:05 +0000

    kkkk

---
+++ .vs/FilmReviewWebsite/FilmReviewWebsite/Controllers/HomeController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FilmReviewWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Our Film Review Website application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Our Film Review Website contact page.";
+
+            return View();
+        }
+
+		public ActionResult Top()
+		{
+			ViewBag.Message = "Our Film Review Website contact page.";
+
+			return View();
+		}
+		public ActionResult Godfather()
+		{
+			ViewBag.Message = "Our Film Review Website contact page.";
+
+			return View();
+		}
+		
+			public ActionResult Shawshank()
+		{
+			ViewBag.Message = "Our Film Review Website contact page.";
+
+			return View();
+		}
+
+		public ActionResult Godfather2()
+		{
+			ViewBag.Message = "Our Film Review Website contact page.";
+
+			return View();
+		}
+		public ActionResult Popular()
+		{
+			ViewBag.Message = "Our Film Review Website contact page.";
+
+			return View();
+		}
+		public ActionResult Green()
+		{
+			ViewBag.Message = "Our Film Review Website contact page.";
+
+			return View();
+		}
+		public ActionResult Bohemian()
+		{
+			ViewBag.Message = "Our Film Review Website contact page.";
+
+			return View();
+		}
+		public ActionResult Star()
+		{
+			ViewBag.Message = "Our Film Review Website contact page.";
+
+			return View();
+		}
+
+		public ActionResult Coming()
+		{
+			ViewBag.Message = "Our Film Review Website contact page.";
+
+			return View();
+		}
+		
+	
+			public ActionResult Actor()
+		{
+			ViewBag.Message = "Our Film Review Website contact page.";
+
+			return View();
+		}
+		public ActionResult Director()
+		{
+			ViewBag.Message = "Our Film Review Website contact page.";
+
+			return View();
+		}
+		public ActionResult Coppola1()
+		{
+			ViewBag.Message = "Our Film Review Website contact page.";
+
+			return View();
+		}
+		public ActionResult Darabont()
+		{
+			ViewBag.Message = "Our Film Review Website contact page.";
+
+			return View();
+		}
+		public ActionResult Farrelly()
+		{
+			ViewBag.Message = "Our Film Review Website contact page.";
+
+			return View();
+		}
+		     
+		public ActionResult Marlon()
+		{
+			ViewBag.Message = "Our Film Review Website contact page.";
+
+			return View();
+		}
+		public ActionResult James()
+		{
+			ViewBag.Message = "Our Film Review Website contact page.";
+
+			return View();
+		}
+		public ActionResult Alfredo()
+		{
+			ViewBag.Message = "Our Film Review Website contact page.";
+
+			return View();
+		}
+
+		public ActionResult Weman()
+		{
+			ViewBag.Message = "Our Film Review Website contact page.";
+
+			return View();
+		}
+		public ActionResult MIB()
+		{
+			ViewBag.Message = "Our Film Review Website contact page.";
+
+			return View();
+		}
+		public ActionResult man()
+		{
+			ViewBag.Message = "Our Film Review Website contact page.";
+
+			return View();
+		}
+
+	}
+}
